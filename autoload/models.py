e2829d859c1e3b8501e58e3356d6393f4d842090
Andrey Komendantov <komandr@gmail.com>
2022-12-06 15:31:38 +0300

    feat: added models of dump trucks

--- autoload/models.py
+++ autoload/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class TruckModel(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    load_capacity = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class DumpTruck(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    model = models.ForeignKey(
+        TruckModel,
+        on_delete=models.CASCADE,
+        related_name='dumptruck'
+    )
+    side_number = models.CharField(max_length=30, unique=True, blank=True)
+
+    def __str__(self):
+        return f"Самосвал №{self.side_number}, модель {self.model}"
